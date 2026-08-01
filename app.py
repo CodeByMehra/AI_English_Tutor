83239df5401bd89478aba22deb7fddce530ce639
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-01 15:41:28 +0530

    basic structure

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 # File for streamlit
 import streamlit as st
-from 
+from src.screens.login_screen import login_screen
 
 def main():
     st.header("English Tutor")
     
     if st.button("Start"):
+        login_screen()
         
         
 main()
--- src/screens/login_screen.py
+++ src/screens/login_screen.py
@@ -1,15 +1,16 @@
 import streamlit as st
 
 
-col1, col2 = st.columns 
+def login_screen():
+    col1, col2 = st.columns 
 
-with col1:
-    st.image
-    
-with col2:
-    st.header("Login")
-    st.text_input("Enter your username")
-    st.text_input("Enter Password")
-    
-    st.text("Dont have an account?")
-    st.link_button("Sign up")
+    with col1:
+        st.image
+        
+    with col2:
+        st.header("Login")
+        st.text_input("Enter your username")
+        st.text_input("Enter Password")
+        
+        st.text("Dont have an account?")
+        st.link_button("Sign up")
