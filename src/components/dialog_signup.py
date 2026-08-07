1a9a92da0c2967211e3cf1fa4dcf4ea6dcdc5718
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-07 14:50:06 +0530

    working on signup

--- src/components/dialog_signup.py
+++ src/components/dialog_signup.py
@@ -2,6 +2,7 @@ import streamlit as st
 from src.database.db import signup_user
 
 def signup_dialog():
+    st.header("you are at signup")
     email = st.text_input("Enter Email")
     password = st.text_input("Enter Password")
     signup_user(email, password)
--- src/screens/login_screen.py
+++ src/screens/login_screen.py
@@ -3,16 +3,24 @@ from src.components.dialog_signup import signup_dialog
 
 
 def login_screen():
-    col1, col2 = st.columns(2)
+    if "signup" not in st.session_state.screen:
+        st.session_state.screen = "signup"
+        
+    if st.session_state.screen == "signup":
+        
+        col1, col2 = st.columns(2)
 
-    with col1:
-        st.image
-        
-    with col2:
-        st.header("Login")
-        st.text_input("Enter your username")
-        st.text_input("Enter Password")
-        
-        st.text("Dont have an account?")
-        if st.button("Sign up") :
-            signup_dialog()
+        with col1:
+            st.image
+            
+        with col2:
+            st.header("Login")
+            st.text_input("Enter your username")
+            st.text_input("Enter Password")
+            
+            st.text("Dont have an account?")
+            if st.button("Sign up") :
+                st.session_state.screens = "signudialog"
+                
+    elif st.session_state.screen == "signupdialog":
+        signup_dialog()
