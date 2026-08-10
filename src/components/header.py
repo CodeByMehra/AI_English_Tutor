e9038ab8462c10b6fa3d9d416274b6bf855a2f9d
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-10 17:14:47 +0530

    changes in header file

--- src/components/header.py
+++ src/components/header.py
@@ -1,4 +1,22 @@
 import streamlit as st
 
-def header():
-    st.header("AI English Tutor")
+def header_home():
+    logo_url = "Link Here"
+    st.markdown(f"""
+        <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; margin-bottom:30px; margin-top:30px">
+            <img src="{logo_url}" style="height:100px; border-radius:15px;">
+            <h1 style="text-align: center; color: #E0E3FF">Attend<br/>Sense</h1>
+        </div>
+    """, unsafe_allow_html=True)
+    
+
+def header_dashboard():
+
+    logo_url = "Link Here"
+    
+    st.markdown(f"""
+        <div style="display:flex; align-items:center; justify-content:center; gap:10px">
+            <img src='{logo_url}' style='height:85px; border-radius:15px;' />
+            <h2 style='text-align:left; color:#5865F2'>Attend<br/>Sense</h2>
+        </div>   
+                """, unsafe_allow_html=True)
