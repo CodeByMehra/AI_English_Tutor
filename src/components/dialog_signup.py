fee8920f6983ff3c1d6bdd4a4ec39aff1df67865
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-10 13:32:42 +0530

    fixed the signup bug

--- src/components/dialog_signup.py
+++ src/components/dialog_signup.py
@@ -3,9 +3,10 @@ from src.database.db import signup_user
 
 def signup_dialog():
     if st.session_state.screen == "signup":
-        st.header("you are at signup page")
-        name = "Enter yout name"
-        username = "Create Username"
+        st.header("you are at signup")
+        
+        name = st.text_input("Enter yout name")
+        username = st.text_input("Create Username")
         email = st.text_input("Enter Email")
         password = st.text_input("Enter Password")
         if st.button("Submit", type="primary"):
