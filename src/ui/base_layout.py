2db110a58f634c4a80196a83d56abbb6ec71cfe1
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-11 15:01:57 +0530

    styled buttons

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -5,13 +5,23 @@ def style_base_layout():
     st.markdown("""
         <style>
         #MainMenu, footer, header { visibility: hidden; }
+        
+        button {
+                background-color: #2F7D5C !important;
+                color: white !important;
+                border-radius: 10px !important;
+                border: none !important;
+                }
         </style>
+
     """, unsafe_allow_html=True)
+    
+    
 
 def style_background_home():
      st.markdown("""
         <style>
-        .stApp { background: #FAFAF9 !important; }
+        .stApp { background:#E8F0E9 !important; }
         </style>
     """, unsafe_allow_html=True)
 
