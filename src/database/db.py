145f5736c4bf7f9998d7d39d0101859cf54dca2c
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-29 13:10:42 +0530

    doing the user details integration

--- src/database/db.py
+++ src/database/db.py
@@ -5,3 +5,7 @@ import bcrypt
 def signup_user(name, username, email, password):
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     print(hashed)
+    supabase.table("users").insert({
+    "email": email,
+    "password": hashed.decode('utf-8')
+    }).execute()
