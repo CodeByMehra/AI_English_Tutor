e5394b880704b3424a8a93dffc18fce7515dc822
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-29 13:28:51 +0530

    exception handling

--- src/database/db.py
+++ src/database/db.py
@@ -4,10 +4,13 @@ import bcrypt
 
 def signup_user(name, username, email, password):
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-    print(hashed)
-    supabase.table("users").insert({
-    "name": name,
-    "username": username,
-    "email": email,
-    "password": hashed.decode('utf-8')
-    }).execute()
+    try:
+        supabase.table("users").insert({
+            "name": name,
+            "username": username,
+            "email": email,
+            "password": hashed.decode('utf-8')
+        }).execute()
+        return True, "Signup successful"
+    except Exception as e:
+        return False, "Username or email already exists"
