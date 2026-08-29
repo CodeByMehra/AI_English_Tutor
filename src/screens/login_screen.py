5f02a3d692dc818f1bede571837b9fbe8a53583d
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-29 14:15:53 +0530

    added side imagr

--- src/screens/login_screen.py
+++ src/screens/login_screen.py
@@ -6,23 +6,25 @@ from src.components.footer import footer_dashboard
 def login_screen():
     if "login" not in st.session_state.screen:
         st.session_state.screen = "login"
-        
+
     if st.session_state.screen == "login":
-        
+
         col1, col2 = st.columns(2)
 
         with col1:
-            st.image
-            
+            st.image("https://i.ibb.co/gZQr6T6k/login-illustration.png", use_container_width=True)
+
         with col2:
             st.header("Login")
-            st.text_input("Enter your username")
-            st.text_input("Enter Password")
-            
+            username = st.text_input("Enter your username")
+            password = st.text_input("Enter Password", type="password")
+
+            if st.button("Log in", type="primary"):
+                st.write("Login logic goes here")
+
             st.text("Dont have an account?")
-            if st.button("Sign up") :
+            if st.button("Sign up", type="secondary"):
                 st.session_state.screen = "signup"
                 st.rerun()
-                
+
         footer_dashboard()
-        
