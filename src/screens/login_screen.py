469b95125bbe316623ed9ae27a0b34c20a109f65
CodeByMehra <kpvishalmehra@gmail.com>
2026-08-30 10:44:44 +0530

    done for the day

--- src/screens/login_screen.py
+++ src/screens/login_screen.py
@@ -32,4 +32,4 @@ def login_screen():
             if st.button("Sign up", type="secondary"):
                 st.session_state.screen = "signup"
                 st.rerun()
-        footer_dashboard() 
+        footer_dashboard()
