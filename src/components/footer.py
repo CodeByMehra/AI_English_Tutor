c0538bdbe7d97ed076092d2f7f6932a738f532f7
CodeByMehra <kpvishalmehra@gmail.com>
2026-09-03 12:58:33 +0530

    fixed typo

--- src/components/footer.py
+++ src/components/footer.py
@@ -16,7 +16,7 @@ def footer_dashboard():
     logo_url = "Link Here"
     
     st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-itemsn:center">
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
         <p style="font-weight:bold; color:black;"> Created by Vishal Mehra</p>  
         </div>
                 
