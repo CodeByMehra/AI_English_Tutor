82ce145837f7fc07d2149211e7c31dc9d945f33e
CodeByMehra <kpvishalmehra@gmail.com>
2026-09-09 16:05:06 +0530

    reverted the changes

--- src/services/transcription.py
+++ src/services/transcription.py
@@ -5,6 +5,7 @@ from groq import Groq
 def transcribe_audio(audio_file_path):
     """Transcribes audio using Groq Whisper API."""
     client = Groq(api_key= st.secrets["GROQ_API_KEY"])
+    
     with open(audio_file_path, "rb") as file:
         transcription = client.audio.transcriptions.create(
             file=(audio_file_path, file.read()),
