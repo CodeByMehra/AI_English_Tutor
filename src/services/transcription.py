60c7de98a54af07902c15d05d72c77312b46a8a2
CodeByMehra <kpvishalmehra@gmail.com>
2026-09-09 15:42:33 +0530

    handeling the api key

--- src/services/transcription.py
+++ src/services/transcription.py
@@ -1,9 +1,10 @@
+import streamlit as st
 import os
 from groq import Groq
 
 def transcribe_audio(audio_file_path):
     """Transcribes audio using Groq Whisper API."""
-    client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+    client = Groq(api_key= st.secrets["GROQ_API_KEY"])
     with open(audio_file_path, "rb") as file:
         transcription = client.audio.transcriptions.create(
             file=(audio_file_path, file.read()),
