445727c71dba6a2a63e87d5ab081e464a8c5088a
CodeByMehra <kpvishalmehra@gmail.com>
2026-09-18 12:42:17 +0530

    phase 1 ready

--- app.py
+++ app.py
@@ -1,37 +1,34 @@
 # File for streamlit
 import streamlit as st
-from src.screens.login_screen import login_screen 
+from src.screens.login_screen import login_screen
 from src.screens.user_screen import user_screen
+from src.screens.conversation_screen import conversation_screen
 from src.components.dialog_signup import signup_dialog
 from src.components.header import header_home
-from src.components.footer import footer_home
-from src.ui.base_layout import style_background_home ,  style_base_layout
+from src.ui.base_layout import style_background_home, style_base_layout
 
-from src.screens.conversation_screen import conversation_screen
 
 def main():
     style_background_home()
     style_base_layout()
-   
+
     if "screen" not in st.session_state:
         st.session_state.screen = "start"
 
     if st.session_state.screen == "start":
         header_home()
         st.header("Your Personalized English Tutor")
-        
-        
-        if st.button("Start"):
+
+        if st.button("Start", type="primary"):
             st.session_state.screen = "login"
             st.rerun()
-        footer_home()
 
-    elif st.session_state.screen == "login":
+    elif st.session_state.screen in ("login", "signup"):
         login_screen()
-    elif st.session_state.screen == "signup":
-        signup_dialog()
-    elif st.session_state.screen == "userscreen": 
+    elif st.session_state.screen == "userscreen":
         user_screen()
     elif st.session_state.screen == "conversation":
         conversation_screen()
+
+
 main()
